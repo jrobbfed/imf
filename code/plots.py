b8782fec977682719d0a4c8639327a445ffeda63
jrobbfed <jrobbfed@gmail.com>
2014-08-04 23:25:26 -0400

    MtoL plots distribution of M/L in IMF samples.

--- code/plots.py
+++ code/plots.py
@@ -1,21 +1,34 @@
 #Make various plots investigating the effects of stochasticity 
 #sampling the IMF
 import numpy as np
-import matplotib.pyplot as plt
+import matplotlib.pyplot as plt
 import imf
 
-def MtoL(plotfile='MtoL', niters=2000., iso='girardi02', imf='larson',
-        mtot = [5e2, 1e3, 3e3, 1e4, 4e4]):
+def MtoL(plotfile='MtoL.pdf', niters=2000, iso='girardi02', massfunc='larson',
+        samplefunc='mc', total_masses=[5e2, 1e3, 3e3, 1e4, 4e4]):
     """
     Replicate Figure 1 of Hernandez et al (2012), which shows the effect
     of stochastic sampling on the distribution of M/L ratios. The default 
     parameters are those used by Hernandez. 
     """
-    mtot = np.array(mtot)
-
+    bins = np.linspace(0, 15, 50)
+    xlabel = r'M/L$_V$'
+    ylabel = 'Number'
 
     f, ax = plt.subplots(1)
 
-    xlabel = 'M/L'
-    ylabel = 'Number'
-
+    for i in range(len(total_masses)):
+        mtot = total_masses[i]
+        ML = np.array([])
+        for j in range(niters):
+            m = imf.sample_imf(mtot, massfunc=massfunc, samplefunc=samplefunc)
+            MLnew = imf.getML(m, iso=iso, mag=True)
+            # print MLnew
+            ML = np.concatenate([ML, [MLnew]])
+        ax.hist(ML, bins=bins, histtype='step', label=str(total_masses[i]) + r' M$_\odot$')
+           
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.legend()
+    ax.set_title('Distribution of M/L in '+str(niters)+' realizations of '+massfunc.capitalize()+' IMF.')
+    plt.savefig(plotfile) 
